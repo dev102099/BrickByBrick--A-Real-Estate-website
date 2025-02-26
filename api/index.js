9309a632dfcfbf9f1d57f19fbcee419eb75ad1ec
Dev Parpyani <sonofhades10@Devs-MacBook-Air.local>
2025-02-26 21:23:03 +0530

    fixes

--- api/index.js
+++ api/index.js
@@ -39,10 +39,7 @@ app.listen(3000, () => {
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/listing", listRoute);
-app.use(express.static(path.join(__dirname, "Client/dist")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server error";
